657dfbbf07b1ae27f0b2992e75d0c57b37928057
gustavo-nomelini <gustavolnomelini@gmail.com>
2025-05-21 10:48:36 -0300

    feat(usuarios): enhance CreateOrganizadorDto with CNPJ validation and improved documentation

--- src/modules/usuarios/dtos/create-organizador.dto.ts
+++ src/modules/usuarios/dtos/create-organizador.dto.ts
@@ -1,6 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, MaxLength} from 'class-validator';
 import { CreateUsuarioDto } from './create-usuario.dto';
+import { Transform } from 'class-transformer';
 
 export class CreateOrganizadorDto extends CreateUsuarioDto {
   @ApiProperty({
@@ -12,22 +13,35 @@ export class CreateOrganizadorDto extends CreateUsuarioDto {
   @MaxLength(100)
   nomeEmpresa: string;
 
+  //Aceitará CNPJs com ou sem formatação
+  //Transformará automaticamente o valor para conter apenas números
+  //Validará se o formato está correto
+  //Armazenará apenas os 14 dígitos numéricos no banco de dados
   @ApiProperty({
-    example: '12345678901234',
-    description: 'CNPJ da empresa ou organização',
+    example: '12.345.678/0001-95',
+    description: 'CNPJ da empresa ou organização (com ou sem formatação)',
   })
   @IsString()
   @IsNotEmpty()
-  @MaxLength(14)
+  @Transform(({ value }) => value.replace(/[^\d]/g, ''))
+  @Matches(/^(\d{14}|\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2})$/, {
+    message: 'CNPJ deve estar no formato correto e conter 14 dígitos numéricos',
+  })
   cnpj: string;
 
+
   @ApiProperty({
-    required: false,
-    example: 'Site da empresa',
+    example: 'https://empresa.com',
     description: 'URL do site da empresa ou organização',
   })
   @IsOptional()
   @IsString()
-  @MaxLength(255)
+  @MaxLength(512)
   site?: string;
+
 }
+
+function Matches(arg0: RegExp, arg1: { message: string; }): (target: CreateOrganizadorDto, propertyKey: "cnpj") => void {
+  throw new Error('Function not implemented.');
+}
+
